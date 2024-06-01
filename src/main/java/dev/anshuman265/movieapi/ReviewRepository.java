91061739c8612e67c813f384cfc45adcba6cd15d
Anshuman <verma77712@gmail.com>
2024-06-02 00:42:14 +0530

    added getByimdbID api

--- src/main/java/dev/anshuman265/movieapi/ReviewRepository.java
+++ src/main/java/dev/anshuman265/movieapi/ReviewRepository.java
@@ -1,15 +1,10 @@
 package dev.anshuman265.movieapi;
 
+
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
-
 @Repository
-public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
-// Code to find the imdB movie by id
-
-    Optional<Movie> findMovieByImdbId(String imdbId);
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
 }
---
+++ src/main/java/dev/anshuman265/movieapi/ReviewService.java
@@ -0,0 +1,11 @@
+package dev.anshuman265.movieapi;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+    public Review createReview(String reviewBody,String imdbId){
+        Review review = new Review();
+    }
+}
